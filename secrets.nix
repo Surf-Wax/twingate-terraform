593f7043d87773baf89c86c275d3988aecbd4f10
Bryce Thorpe <145292208+Surf-Wax@users.noreply.github.com>
2024-07-30 15:00:20 -0700

    Update secrets.nix with comments

--- secrets.nix
+++ secrets.nix
@@ -1,3 +1,8 @@
+# secrets.nix
+# This is where you will input your twingate information, including API Key
+# NOTE: if this file is not encrypted or otherwise protected,
+#  theese variables are plaintext and need to be handled carefully
+# These values will be exported into environment variables: see flake.nix
 {
   twingateApiKey = "";
   twingateNetwork = "";
